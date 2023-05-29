35bff388127b0d8460e178b46556d354e5f11c87
Chiocheti <gabrielchiocheti@gmail.com>
2023-05-29 18:07:23 -0300

    feat(seed): define the seeds of the shows

---
+++ api/seeders/20230529190243-demo-shows.js
@@ -0,0 +1,79 @@
+const { v4: uuidv4 } = require('uuid');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('shows', [
+      {
+        id: uuidv4(),
+        show_name: 'Edson e Hudson',
+        tickets_left: 800,
+        show_day: '2023-06-30'
+      },
+      {
+        id: uuidv4(),
+        show_name: 'MC Don Juan',
+        tickets_left: 800,
+        show_day: '2023-06-30'
+      },
+      {
+        id: uuidv4(),
+        show_name: 'Gusttavo Lima',
+        tickets_left: 800,
+        show_day: '2023-07-01'
+      },
+      {
+        id: uuidv4(),
+        show_name: 'Guilherme e Bennuto',
+        tickets_left: 800,
+        show_day: '2023-07-02'
+      },
+      {
+        id: uuidv4(),
+        show_name: 'Noite Gospel',
+        tickets_left: 800,
+        show_day: '2023-07-06'
+      },
+      {
+        id: uuidv4(),
+        show_name: 'Thiaguinho',
+        tickets_left: 800,
+        show_day: '2023-07-07'
+      },
+      {
+        id: uuidv4(),
+        show_name: 'Hugo e Guilherme',
+        tickets_left: 800,
+        show_day: '2023-07-07'
+      },
+      {
+        id: uuidv4(),
+        show_name: 'Luan Santana',
+        tickets_left: 800,
+        show_day: '2023-07-08'
+      },
+      {
+        id: uuidv4(),
+        show_name: 'Dennis DJ',
+        tickets_left: 800,
+        show_day: '2023-07-08'
+      },
+      {
+        id: uuidv4(),
+        show_name: 'Trio Parada Dura',
+        tickets_left: 800,
+        show_day: '2023-07-09'
+      },
+      {
+        id: uuidv4(),
+        show_name: 'Bruna Viola',
+        tickets_left: 800,
+        show_day: '2023-07-09'
+      },
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('shows', null, {});
+  }
+};
