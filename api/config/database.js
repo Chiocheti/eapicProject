c32236d9302d104ae8afb52b262fa13394a42533
Cleber Julimari de Carvalho <43710794+gitdocleber@users.noreply.github.com>
2023-06-02 09:31:18 -0300

    Configure Postgres

--- api/config/database.js
+++ api/config/database.js
@@ -24,5 +24,11 @@ module.exports = {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       dialect: process.env.DB_DIALECT,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
     },
   };
